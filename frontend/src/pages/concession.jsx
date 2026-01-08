99ed60546d48f71ff2f98d79a79393d9456e3294
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2026-01-08 21:17:09 +0530

    Enhance Concession page layout with Navbar and styling

--- frontend/src/pages/concession.jsx
+++ frontend/src/pages/concession.jsx
@@ -1,6 +1,8 @@
+import Navbar from '../components/Navbar';
 export default function Concession() {
   return (
-    <div>
+    <div className="min-h-screen bg-background-900">
+      <Navbar />
       <h1>Concession Page</h1>
       {/* Concession page content goes here */}
     </div>
